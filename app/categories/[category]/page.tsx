95c850f283791957e1bcc1aaaa062da2308d688f
Hadia Faisal <hadiafaisal1003@gmail.com>
2025-01-22 17:22:26 +0500

    fixed url in categories

--- app/categories/[category]/page.tsx
+++ app/categories/[category]/page.tsx
@@ -9,7 +9,7 @@ const CategoryPage = async ({ params }: Props) => {
   const { category } = await params; // Await the params to destructure the category
 
   // Fetch blogs on the server side, passing the category parameter
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/blog?category=${category}`);
+  const response = await fetch(`https://jdntuition-596yvk2hw-hadia-faisals-projects.vercel.app/api/blog?category=${category}`);
   const data = await response.json();
 
   // Map category format to display-friendly format
