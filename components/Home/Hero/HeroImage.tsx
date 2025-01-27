e146c5259514676b94ac686cfe37658e69676777
Hadia Faisal <hadiafaisal1003@gmail.com>
2025-01-27 17:14:14 +0500

    updated the hero section

--- components/Home/Hero/HeroImage.tsx
+++ components/Home/Hero/HeroImage.tsx
@@ -4,11 +4,20 @@ import React from "react"
 
 const HeroImage = () => {
   return (
-    <div data-aos="fade-up" data-aos-delay="1000" className="w-full h-full">
-      <Image src="/images/hero-1.png" width={700} height={500} alt="Hero" className="object-contain" />
+    <div 
+      data-aos="fade-up" 
+      data-aos-delay="1000" 
+      className="w-full h-auto flex justify-center lg:justify-end"
+    >
+      <Image 
+        src="/images/hero-1.png" 
+        width={700} 
+        height={500} 
+        alt="Hero" 
+        className="object-contain max-w-[100%] sm:max-w-[100%] md:max-w-[100%] lg:max-w-[70%] xl:max-w-[90%]" 
+      />
     </div>
   )
 }
 
 export default HeroImage
-
