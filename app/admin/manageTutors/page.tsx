cdc802a0dd5e6f6f1cb5db5d28b55887e42e8516
Hadia Faisal <hadiafaisal1003@gmail.com>
2025-02-20 20:27:17 +0500

    fixed deployment error

--- app/admin/manageTutors/page.tsx
+++ app/admin/manageTutors/page.tsx
@@ -76,6 +76,7 @@ const ManageTutors = () => {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '',
+    libraries: ['places', 'maps']
   });
 
   useEffect(() => {
